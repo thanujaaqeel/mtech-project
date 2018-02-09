85477206afb67f69b0b4e683caf0b7ea0c6386d0
Thanuja Mohamadali <thanuja.mohamadali@gmail.com>
2018-02-09 14:14:36 +0530

    basic publisher class

---
+++ twitter-input/publisher.py
@@ -0,0 +1,25 @@
+import redis
+import time
+
+class Publisher():
+  def __init__(self, channel, rate):
+    self.channel = channel
+    self.redis_cli = redis.Redis()
+    self.rate = rate
+
+  def publish(self, message):
+    self.redis_cli.publish(self.channel, message)
+
+  def start_publishing_from(self, source_file):
+    with open(source_file, "r") as fp:
+      for line in fp:
+          self.publish(line)
+          time.sleep(self.rate)
+
+CHANNEL = "TWITTER_STREAM"
+FILE = "tweets.txt"
+RATE = 0.2
+
+if __name__ == "__main__":
+  publisher = Publisher(CHANNEL, RATE)
+  publisher.start_publishing_from(FILE)
